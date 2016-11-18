f993c024f15c645785f37413d397bec8effd4c66
Matthias Tietz <tietz_matthias@yahoo.de>
2016-11-18 10:54:06 +0100

    add delete funct. & correct display of new gb entries

--- ue4/script.js
+++ ue4/script.js
@@ -11,6 +11,12 @@ window.onload = function () {
     listTop =document.getElementsByTagName("ul")[0];
     vsrUrl = document.getElementsByTagName("form")[0].action;
 
+    // event listener
+    document.getElementsByTagName("button")[0].addEventListener("click", function (event) {
+            event.preventDefault();
+            SubmitEntry();
+    });
+
     // onLoad event
     var xHttp = new XMLHttpRequest();
 
@@ -24,8 +30,8 @@ window.onload = function () {
     xHttp.send();
 }
 
-function renderGbAtStart(gbObject) {
-    allGbEntries = gbObject;
+function renderGbAtStart(gbResponse) {
+    allGbEntries = gbResponse;
 
     // process the js object
     for (var i = 0; i < allGbEntries.length; i++) {
@@ -47,16 +53,58 @@ function createGbEntry(jsObject) {
     xHref.href = "#";
     xHref.alt = "Delete entry";
     xHref.addEventListener("click", function () {
-        // remove childs
-        while (liEntry.firstChild) {
-            liEntry.removeChild(liEntry.firstChild);
-        }
-        // after that remove the parent node (li)
-        listTop.removeChild(liEntry);
+        removeElement(liEntry, jsObject.id)
     });
 
     liEntry.appendChild(divName);
     liEntry.appendChild(divText);
     liEntry.appendChild(xHref);
     return liEntry;
+}
+
+function removeElement(liEntry, id) {
+    // delete entry from remote gb
+    var idPara = "id=" + id;
+    var xHttpR = new XMLHttpRequest();
+    xHttpR.open("DELETE", vsrUrl + "?" + idPara, true);
+    xHttpR.send();
+
+    // remove childs ("local")
+    while (liEntry.firstChild) {
+        liEntry.removeChild(liEntry.firstChild);
+    }
+    // after that remove the parent node (li)
+    listTop.removeChild(liEntry);
+}
+
+function SubmitEntry() {
+    var nameV = document.getElementById("name").value;
+    var textV = document.getElementById("text").value;
+    var toSend =  {"name":nameV, "text":textV};
+
+    // error cases
+    if (nameV === "") {
+        alert("Please provide a name.");
+        return;
+    }
+    if (textV === "") {
+        alert("Please provide some text.");
+        return;
+    }
+
+    var xHttp = new XMLHttpRequest();
+    xHttp.open("POST", vsrUrl, true);
+    xHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xHttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            // append Entry
+            // JSON.parse(this.responseText)
+            var newItem = createGbEntry(toSend);
+            listTop.insertBefore(newItem, listTop.childNodes[0]);
+        }
+    };
+
+    xHttp.send(toSend);
 }
